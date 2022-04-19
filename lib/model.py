aaa80906139b23ae7900e6975a30dd0fa8ccf94b
Damian Mikoláš <damo3433@gmail.com>
2022-04-19 21:07:59 +0200

    Add database types

--- lib/model.py
+++ lib/model.py
@@ -1,29 +1,24 @@
-import discord
-from pymongo import MongoClient
-from os import getenv
+from typing import TypedDict, Any
 
 
-class Database:
+class SlowmodeType(TypedDict):
+    user_id: int
+    duration: int
+    duration_unit: str
+    interval: int
+    interval_unit: str
+    reason: Any
+    channel_id: Any
+    issued_by: str
+    issued_at: float
 
-    def __init__(self):
-        self.client = MongoClient(getenv("DATABASE_URL"))
-        self.db = self.client[getenv("DATABASE_NAME")]
-        self.events = self.db["events"]
-        self.shutdowns = self.db["shutdowns"]
-        self.slowmode = self.db["users_slowmode"]
 
-    def all_events(self):
-        return list(self.events.find({}))
+class Event(TypedDict):
+    name: str
+    author_id: int
+    description: str
+    time: int
+    role: Any
+    utc_time: float  # TODO int?
 
-    def all_shutdowns(self):
-        return list(self.shutdowns.find({}))
-
-    def all_slowmodes(self):
-        return list(self.slowmode.find({}))
-
-    def user_has_slowmode(self, user: discord.User):
-        return self.slowmode.find_one({'user_id': user.id}) is not None
-
-    def event_exists(self, name: str):
-        return self.events.find_one({'name': name.strip().replace(' ', '_')}) is not None
 
